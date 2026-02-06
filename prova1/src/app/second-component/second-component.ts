56ec680a32138532dd6088046ab201d99b51a33d
Alexandra Gumalal <gumalal.alexandra@iisgalvanimi.edu.it>
2026-02-06 06:13:35 +0000

    prova di generazione compoenente andata bene

--- prova1/src/app/second-component/second-component.ts
+++ prova1/src/app/second-component/second-component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { FourthComponent } from '../fourth-component/fourth-component';
 
 @Component({
   selector: 'app-second-component',
-  imports: [],
+  imports: [FourthComponent],
   templateUrl: './second-component.html',
   styleUrl: './second-component.css',
 })
 export class SecondComponent {
-
+name = 'boh';
 }
